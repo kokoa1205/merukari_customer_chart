4b0f7cb28ce4f921d516ca6baf9b8dbb44a00f7d
ruku <ritorito0715@gmail.com>
2021-05-19 10:23:30 +0900

    master

--- index.php
+++ index.php
@@ -13,23 +13,23 @@ $query_builder = TRUE;
 // connect DB
 $conn = mysqli_connect($cleardb_server,$cleardb_username,$cleardb_password,$cleardb_db);
 
-$sql = "SELECT name, message FROM member WHERE name=? AND message=?";
-if ($stmt = $mysqli->prepare($sql)) {
-    // 条件値をSQLにバインドする（補足参照）
-    $user_id = 123;
-    $name = "hanako";
-    $stmt->bind_param("is", $user_id, $name);
+// $sql = "SELECT name, message FROM member WHERE name=? AND message=?";
+// if ($stmt = $mysqli->prepare($sql)) {
+//     // 条件値をSQLにバインドする（補足参照）
+//     $user_id = 123;
+//     $name = "hanako";
+//     $stmt->bind_param("is", $user_id, $name);
 
-    // 実行
-    $stmt->execute();
+//     // 実行
+//     $stmt->execute();
 
-    // 取得結果を変数にバインドする
-    $stmt->bind_result($user_id, $name);
-    while ($stmt->fetch()) {
-        echo "ID=$user_id, NAME=$name<br>"; 
-    }
-    $stmt->close();
-}
+//     // 取得結果を変数にバインドする
+//     $stmt->bind_result($user_id, $name);
+//     while ($stmt->fetch()) {
+//         echo "ID=$user_id, NAME=$name<br>"; 
+//     }
+//     $stmt->close();
+// }
 
 
 echo "Hello world heroku";
