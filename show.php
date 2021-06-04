05655bbb608d4a2d0b2396ba2ef1bd4ae300ab3b
ruku <ritorito0715@gmail.com>
2021-06-04 19:44:02 +0900

    master

--- show.php
+++ show.php
@@ -38,6 +38,7 @@ $stmt = $db->query($sql);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="./main.js"></script>
+  <link rel="stylesheet" href="../css/show.css">
   <title>Document</title>
 </head>
 <body>
