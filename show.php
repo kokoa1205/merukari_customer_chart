60f771346b1e91fe42f2052c396b9aa363d5a959
ruku <ritorito0715@gmail.com>
2021-06-15 13:24:46 +0900

    a

--- show.php
+++ show.php
@@ -38,7 +38,8 @@ $stmt = $db->query($sql);
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="./main.js"></script>
   <link rel="stylesheet" href="../css/show.css">
@@ -48,7 +49,7 @@ $stmt = $db->query($sql);
 <style>
 @media screen and (max-width: 480px) {
   table{
-    width: 300px;
+    width: 100px;
     margin: 20px;
   }
 }
