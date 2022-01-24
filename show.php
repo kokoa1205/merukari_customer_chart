254ec12d7b13785afb1eb50f93aa288c06af2adb
ruku <ritorito0715@gmail.com>
2022-01-25 07:33:28 +0900

    a

--- show.php
+++ show.php
@@ -53,7 +53,7 @@ foreach ($stmt as $target) {
 $delete_index = array();
 for ($i = 0;$i<count($name);$i++) {
     for ($j = $i+1;$j<count($name);$j++) {
-        if ($name[$i]['name'] == $name[$j]['name'] && $name[$i]['created'] == date('yyyy/mm/dd', strtotime( $name[$j]['created'] . " -1 day")) && $name[$i]['message'] == $name[$j]['message']) {
+        if ($name[$i]['name'] == $name[$j]['name'] && $name[$i]['created'] == date('yyyy-mm-dd', strtotime( $name[$j]['created'] . " -1 day")) && $name[$i]['message'] == $name[$j]['message']) {
             // try {
             //     $delete = "DELETE FROM merukari_member WHERE id = :id";
             //     $delete_stmt = $db->prepare($delete);
